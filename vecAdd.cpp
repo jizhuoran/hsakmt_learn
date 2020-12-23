b2e883e005d4ac8ec825f5d9b77752453d4bdc1f
jizhuoran <jizr@connect.hku.hk>
2020-12-23 17:00:34 +0800

    auto push

--- vecAdd.cpp
+++ vecAdd.cpp
@@ -137,6 +137,7 @@ static amd_dbgapi_callbacks_t dbgapi_callbacks = {
 
 		library_id.handle = (uint64_t)dlopen(library_name, RTLD_NOW);
         *library_state = AMD_DBGAPI_SHARED_LIBRARY_STATE_LOADED;
+		std::cout << "The loaded address is " << (void*) library_id.handle << std::endl;
         return AMD_DBGAPI_STATUS_SUCCESS;
       },
 
